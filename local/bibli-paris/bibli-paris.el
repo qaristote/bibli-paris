260ef4efd5ac330dde21b9fef9a51458501799ba
Quentin Aristote <quentin@aristote.fr>
2021-12-08 12:32:27 +0100

    clean-quote : add examples in comments and debug "1" -> "P1" rule

--- local/bibli-paris/bibli-paris.el
+++ local/bibli-paris/bibli-paris.el
@@ -186,19 +186,25 @@ date if borrowed and set to the maximum unix date if unavailable."
   "Remove extra whitespace from and uniformizes the quote QUOTE."
   (let ((blank "[[:blank:]\r\n]+")
         (result entry-quote))
+    ;; "  " -> " "
     (setq result (replace-regexp-in-string blank " " result))
+    ;; "$ " -> "$"
     (setq result (string-trim result blank blank))
+    ;; "BD EN RESERVE" -> "EN RESERVE BD"
     (setq result (replace-regexp-in-string
                   "BD EN RESERVE" "EN RESERVE BD"
                   result t t))
+    ;; "BD MANGA" -> "MANGA"
     (setq result (replace-regexp-in-string
                   "BD MANGA" "MANGA"
                   result t t))
+    ;; "BD COMICS" -> "COMICS"
     (setq result (replace-regexp-in-string
                   "BD COMICS" "COMICS"
                   result t t))
+    ;; "1" -> "01"
     (setq result (replace-regexp-in-string
-                  "\\([^0-9/]\\)\\([0-9]\\)\\($\\|\/\\)"
+                  "\\([^0-9/]\\)\\([0-9]\\)\\($\\|\/\\| \\)"
                   "\\10\\2\\3"
                   result))
     result))
